b5a222bd51d26141e37d9eb830e1405949809073
small-j <44703262+small-j@users.noreply.github.com>
2020-12-28 12:51:25 +0900

    The end of Chapter2

---
+++ src/main/java/com/smallj/study/springboot/Application.java
@@ -0,0 +1,14 @@
+package com.smallj.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@Component Scan 어노테이션을 가지고 있는 어노테이션.
+//이 파일이 있는 위치에서 하위의 클래스 중 빈을 만들어줘야하는 @Component어노테이션을 가진 클래스를 찾는다.
+@SpringBootApplication
+public class Application {
+    //메인클래스
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/smallj/study/springboot/web/HelloController.java
@@ -0,0 +1,20 @@
+package com.smallj.study.springboot.web;
+
+import com.smallj.study.springboot.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+//현재 컨트롤러를 JSON을 반환하는 컨트롤러로 만들어줌
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
+}
